678a08a3b06c8840dc125529c8ace2fffb44964c
Your Name <you@example.com>
2026-08-01 14:30:17 +0700

    fix: update plugin bootstrap repository catalog URL to DuskCipher

--- lib/services/plugin_bootstrap_service.dart
+++ lib/services/plugin_bootstrap_service.dart
@@ -58,7 +58,7 @@ class PluginBootstrapProgress {
 
 class PluginBootstrapService {
   static const String hostedRepositoriesUrl =
-      'https://hemantkarya.github.io/StreamBeatsTunes/repositories.json';
+      'https://raw.githubusercontent.com/DuskCipher/StreamBeats/master/repositories.json';
 
   static const int maxRetries = 3;
 
